d605488d36da3dc4a0d665ea775cb42bf46acd62
HayyanWasi <hayyanwasi360@gmail.com>
2025-07-04 23:52:44 +0500

    day6

--- day6.py
+++ day6.py
@@ -1,4 +1,12 @@
-from typing import List
-string = list(str)
+string = "hello  world is mad at me i".split()
 
-def reverseWords()->str:
+def reverse_string(string):
+    left, right  = 0, len(string)-1
+    while left<right:
+            string[left], string[right] = string[right], string[left]
+            left+=1
+            right-=1
+
+    return ' '.join(string)
+
+print(reverse_string(string))
